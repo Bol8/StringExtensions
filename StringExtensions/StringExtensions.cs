3dace51e109f2b737914eea3651faec4f863b5b3
Oscar <Oscarbolon@hotmail.com>
2016-09-07 18:31:34 +0200

    Añadidos los métodos clearPrefix,JoinUplast,JoinUpStart

--- StringExtensions/StringExtensions.cs
+++ StringExtensions/StringExtensions.cs
@@ -8,5 +8,46 @@ namespace StringExtensions
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Método que limpia,si existe, el prefijo indicado
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        public static string ClearPrefix(this string source, string prefix)
+        {
+            return source.StartsWith(prefix) ? source.Substring(prefix.Length) : source;
+        }
+
+        /// <summary>
+        /// Método que concatena una cadena a la derecha del string.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="stringToJoin"></param>
+        /// <returns></returns>
+        public static string JoinUpLast(this string source, string stringToJoin)
+        {
+            var newString = new StringBuilder(source);
+            newString.Append(stringToJoin);
+
+            return newString.ToString();
+        }
+
+        /// <summary>
+        /// Método que concatena una cadena a la izquierda del string.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="stringToJoin"></param>
+        /// <returns></returns>
+        public static string JoinUpStart(this string source, string stringToJoin)
+        {
+            var newString = new StringBuilder(stringToJoin);
+            newString.Append(source);
+
+            return newString.ToString();
+        }
+
+
+
     }
 }
